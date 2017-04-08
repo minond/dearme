3588f03fd6d29b4d16f51dde265b563c6188d209
Marcos Minond <minond.marcos@gmail.com>
2017-04-07 20:33:25 -0600

    common styles

---
+++ src/client/styles.ts
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'aphrodite';
+
+export const common = StyleSheet.create({
+    text: {
+        fontSize: '22px',
+        lineHeight: '32px',
+    },
+
+    centered_text: {
+        textAlign: 'center',
+    },
+
+    button: {
+        border: '1px solid #c3c3c3',
+        background: 'white',
+        fontSize: '22px',
+        textTransform: 'uppercase',
+        fontWeight: 'bold',
+        padding: '20px',
+        minWidth: '350px',
+    },
+
+    med_space: {
+        height: '75px',
+    },
+});
--- src/client/welcome.tsx
+++ src/client/welcome.tsx
@@ -1,4 +1,5 @@
 import { StyleSheet, css } from 'aphrodite';
+import { common } from './styles';
 
 import * as React from 'react';
 
@@ -15,44 +16,21 @@ const styles = StyleSheet.create({
         backgroundPosition: '50%',
         width: '650px',
     },
-
-    text: {
-        fontSize: '22px',
-        lineHeight: '32px',
-    },
-
-    centered_text: {
-        textAlign: 'center',
-    },
-
-    button: {
-        border: '1px solid #c3c3c3',
-        background: 'white',
-        fontSize: '22px',
-        textTransform: 'uppercase',
-        fontWeight: 'bold',
-        padding: '20px',
-        minWidth: '350px',
-    },
-
-    med_space: {
-        height: '75px',
-    },
 });
 
 export const WelcomeComponent = ({ onSignUp }: Props) =>
-    <div className={css(styles.centered_text)}>
+    <div className={css(common.centered_text)}>
         <div className={css(styles.dearme_img)}></div>
-        <div className={css(styles.med_space)}></div>
+        <div className={css(common.med_space)}></div>
 
-        <section className={css(styles.text)}>
+        <section className={css(common.text)}>
             <div>The easiest way to journal EVER.</div>
             <div>We send a few texts a day. You answer.</div>
             <div>We add your instagram photos.</div>
             <div>You have a journal.</div>
         </section>
 
-        <div className={css(styles.med_space)}></div>
-        <button className={css(styles.button)}
+        <div className={css(common.med_space)}></div>
+        <button className={css(common.button)}
             onClick={() => onSignUp()}>sign up</button>
     </div>;

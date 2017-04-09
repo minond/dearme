cf66b79a014fdabcdb391fb65ae83261b8e551db
Marcos Minond <minond.marcos@gmail.com>
2017-04-09 13:55:15 -0600

    configuration component

---
+++ src/client/confirm.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { css } from 'aphrodite';
+import { common } from './styles';
+
+type Props = { className?: string; };
+
+export const ConfirmComponent = ({ className }: Props) =>
+    <div className={className}>
+        <div className={css(common.text_centered)}>
+            <div className={css(common.large_text)}>wonderful!</div>
+            <div className={css(common.med_space)}></div>
+
+            <section className={css(common.small_text)}>
+                <div>Check your phone for a confirmation text.</div>
+                <div>The first week of your journal will be delivered to you one week from today.</div>
+            </section>
+
+            <div className={css(common.med_space)}></div>
+        </div>
+    </div>;

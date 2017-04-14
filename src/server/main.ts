ee4b384a7a30b3d5aff308c997193fdb17b6dece
Marcos Minond <marcos.minond@domo.com>
2017-04-13 20:14:13 -0600

    handlers

--- src/server/main.ts
+++ src/server/main.ts
@@ -2,13 +2,12 @@ import * as RateLimit from 'express-rate-limit';
 // import * as Csurf from 'csurf';
 
 import { config, application } from '../application';
+import { handler_save_user, handler_serve_index } from './handler';
 
 const server = application(config);
 const limit = new RateLimit(config<RateLimit.Options>('ratelimit.default'));
 // const csrf = Csurf({ cookie: config<Csurf.CookieOptions>('app.csrf.cookie') });
 
-server.get('/', (req, res) =>
-    res.render('index'));
-
-server.listen(3000, () =>
-    console.log('ready'));
+server.get('/', /*csrf,*/ handler_serve_index);
+server.post('/signup', limit, /*csrf,*/ handler_save_user);
+server.listen(3000, () => console.info('ready for http calls'));

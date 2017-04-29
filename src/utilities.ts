e7affd41e69018548b8c63dd20c785d79b6bda59
Marcos Minond <marcos.minond@domo.com>
2017-04-29 13:04:18 -0600

    duration types

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,3 +1,11 @@
+export type Duration = number;
+
+export const MILLISECOND: Duration = 1;
+export const SECOND = MILLISECOND * 1000;
+export const MINUTE = SECOND * 60;
+export const HOUR = MINUTE * 60;
+export const DAY = HOUR * 24;
+
 export function maybe<T>(val: T | null) {
     return {
         or_else(backup: () => T): T {

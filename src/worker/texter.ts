4fe38fec8bdd1579cedca8fa7dfe7cc1f0696ef7
Marcos Minond <marcos.minond@domo.com>
2017-04-29 22:50:37 -0600

    acking all messages

--- src/worker/texter.ts
+++ src/worker/texter.ts
@@ -1,17 +1,11 @@
 import { config } from '../application';
 import { logger } from '../log';
-import { send, SMSSend, SMSAck } from '../device/sms';
+import { send, SMSSend } from '../device/sms';
 import { channel, parse, LazyChannel } from '../device/amqp';
 import { QueuedMessage } from '../controller/message';
 
 const log = logger(__filename);
 const queue = config<string>('amqp.queues.messages');
-
-const log_error = (err: Error) =>
-    log.error('cound not talk to messaging service', err);
-
-const log_success = () =>
-    log.log('sent text, sent ack');
 
 export function texter(
     sender: SMSSend = send,
@@ -23,16 +17,11 @@ export function texter(
         chan.consume(queue, (msg) => {
             let { phone, body } = parse(msg) as QueuedMessage;
 
-            let ack_message = (ack: SMSAck) =>
-                !ack.error_code ? chan.ack(msg) :
-                    log.error('error from messaging service', ack);
-
             log.info('got message');
 
             sender(phone, body)
-                .then(ack_message)
-                .then(log_success)
-                .catch(log_error);
+                .catch((err) => log.error('cound not talk to messaging service', err))
+                .then((ack) => chan.ack(msg));
         });
     });
 }

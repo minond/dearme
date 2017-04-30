12b319b167ab90fd18a4e7e6e610949c43b6ac95
Marcos Minond <marcos.minond@domo.com>
2017-04-29 22:06:48 -0600

    updating scheduler

--- src/worker/scheduler.ts
+++ src/worker/scheduler.ts
@@ -3,66 +3,71 @@ import { config } from '../application';
 import { mongo, Connecting } from '../device/mongo';
 import { channel, LazyChannel } from '../device/amqp';
 import { logger } from '../log';
-import { user } from '../repository/user';
+import { user, User } from '../repository/user';
 import { schedule } from '../controller/message';
-import { conversation, Conversation } from '../repository/conversation';
+import { message, Message } from '../repository/message';
 
 const log = logger(__filename);
-const completed_convo_query = (err?: Error) =>
-    err ? log.error(err) : log.log('ok');
 
 const range = ms(config<string>('app.worker.scheduler.range'));
 const interval = ms(config<string>('app.worker.scheduler.interval'));
 
+const completed_convo_query = (err?: Error) =>
+    err ? log.error(err) : true;
+
 export async function scheduler(
     connection: Connecting = mongo,
-    messages: LazyChannel = channel.messages
+    messages_chan: LazyChannel = channel.messages
 ) {
     let db = await connection;
     let users = user(db);
-    let conversations = conversation(db);
-    let chan = await messages();
+    let messages = message(db);
+    let chan = await messages_chan();
 
     let now = Date.now();
 
     let $gte = new Date(now - range / 2);
     let $lte = new Date(now + range / 2);
 
-    let fields = {
-        user_id: 1,
-        messages: {
-            $elemMatch: {
-                send_date: { $gte, $lte }
-            }
-        }
+    let query = {
+        send_date: { $gte, $lte },
+        scheduled: false,
+        user_id: { $exists: true },
+        body: { $exists: true },
     };
 
-    let valid_convo = (convo: Conversation): boolean =>
-        !!convo.messages && !!convo.messages.length;
+    let task = () => {
+        let user_cache: { [index: string]: User } = {};
 
-    let task = () => conversations.find({}, fields)
-        .forEach(async (convo: Conversation) => {
-            log.info('processing conversation');
+        messages.find(query)
+            .forEach(async (message: Message) => {
+                let { user_id, _id: message_id, body } = message;
+                let hash = user_id.toHexString();
 
-            if (!valid_convo(convo)) {
-                return;
-            }
+                log.info(`processing Message#${message_id}`);
 
-            let user = await users.findOne({ _id: convo.user_id });
+                let user_filter = { _id: user_id };
+                let message_filter = { _id: message_id };
 
-            convo.messages.map(async (message) => {
+                let user = hash in user_cache ? user_cache[hash] :
+                    await users.find_one(user_filter);
+
                 try {
-                    let ok = await schedule(chan, user, message.body);
-                    log.info(`scheduled message for User#${user._id} ok: ${ok}`);
+                    await messages.update(message_filter, { $set: { scheduled: true } });
+                    log.info(`updated scheduled flag for for User#${user_id}`);
+
+                    let ok = await schedule(chan, user, body);
+                    log.info(`scheduled message for User#${user_id} ok: ${ok}`);
                 } catch (err) {
-                    log.info(`scheduled message for User#${user._id} ok: false`);
+                    log.error(`error handling User#${user_id}`);
                     log.error(err);
                 }
-            });
-        }, completed_convo_query);
+            }, completed_convo_query);
+    };
 
     log.info(`triggering task every ${interval} ms`);
     setInterval(task, interval);
+    task();
 }
 
 if (!module.parent) {

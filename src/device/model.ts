66fb8ac6242e5d69dd72742dec730644a4d98005
Marcos Minond <minond.marcos@gmail.com>
2017-05-10 19:40:17 -0600

    repo helper

--- src/device/model.ts
+++ src/device/model.ts
@@ -1,4 +1,5 @@
 import { Cursor, ObjectID } from '../device/mongo';
+import { Collection } from './mongo';
 
 export type ModelID = ObjectID;
 
@@ -10,8 +11,27 @@ export interface Model {
 export interface Repository<T extends Model> {
     find(query: object, fields?: object): Cursor<T>;
     find_one(query: object): Promise<T>;
-    save(obj: object): Promise<T>;
-    save_many(obj: object[]): Promise<T[]>;
+    save(obj: Partial<T>): Promise<T>;
+    save_many(obj: Partial<T>[]): Promise<T[]>;
     update(filter: object, update: object): Promise<null>;
     [index: string]: _;
+}
+
+export function repo<T>(coll: () => Collection): Repository<T> {
+    return {
+        find: (query: object, fields?: object): Cursor<T> =>
+            coll().find(query, fields),
+
+        find_one: (query: object): Promise<T> =>
+            coll().findOne(query),
+
+        save: (model: Partial<T>): Promise<T> =>
+            coll().insert(model as object).then(() => model),
+
+        save_many: (models: Partial<T>[]): Promise<T[]> =>
+            coll().insertMany(models as object[]).then(() => models),
+
+        update: (filter: object, update: object): Promise<null> =>
+            coll().findOneAndUpdate(filter, update).then(() => null),
+    };
 }
